3ba7ee0619c1c53ad648e555c3f83bbb78f065d0
DmitriyAleksandrovich <113446519+vasovneica@users.noreply.github.com>
2024-07-08 12:06:28 +0400

    Update FullPost.jsx

--- pages/FullPost.jsx
+++ pages/FullPost.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import axios from '../axios';
+import { useParams } from 'react-router-dom';
 import { Post } from '../components/Post';
 import { Index } from '../components/AddComment';
 import { CommentsBlock } from '../components/CommentsBlock';
