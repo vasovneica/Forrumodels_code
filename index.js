233ea5828ec050587d8b2950f4cdb7f086086fe1
DmitriyAleksandrovich <113446519+vasovneica@users.noreply.github.com>
2024-07-08 12:08:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'eact-router-dom';
-import App from './App';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Provider } from 'react-redux';
-import './index.scss';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
+import { BrowserRouter } from 'eact-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+import './index.scss';
 import store from './redux/store';
 
 
