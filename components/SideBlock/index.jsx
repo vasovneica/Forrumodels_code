ef7d6d9724cf6692e0710681ef61b00e5f51c12f
DmitriyAleksandrovich <113446519+vasovneica@users.noreply.github.com>
2024-07-08 12:04:32 +0400

    Update index.jsx

--- components/SideBlock/index.jsx
+++ components/SideBlock/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styles from './SideBlock.module.scss';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import styles from './SideBlock.module.scss';
 
 export const SideBlock = ({ title, children }) => {
 
